8a370bff65555f546491a3dc32cccd166f0355db
tubi55 <tubi55@nate.com>
2022-01-10 23:07:49 +0900

    객체지향으로 코드 변경

--- animate.js
+++ animate.js
@@ -1,65 +1,65 @@
-const btn = document.querySelector('button');
-const box = document.querySelector('#box');
+class Animate {
+    constructor(selector, option){
+        this.selector = selector;
+        this.option = {duration:500, ...option};
+        console.log(this.option);
+        this.startTime= performance.now();
+        this.currentValue= null;
+        this.isString = typeof this.option.value;
+        
+        if(this.option.prop==='scroll'){
+            this.currentValue = this.selector.scrollY || this.selector.pageYOffset;
+        }else{
+            this.currentValue = parseFloat(getComputedStyle(this.selector)[this.option.prop]); 
+        }  
+        
+        if(this.isString === 'string') {
+            const parentW = parseInt(getComputedStyle(this.selector.parentElement).width);
+            const parentH = parseInt(getComputedStyle(this.selector.parentElement).height);
+            const x = ['margin-left','margin-right','left','right','width'];
+            const y = ['margin-top','margin-bottom','top','bottom','height'];
 
-btn.addEventListener('click', e=>{
-    animate(window, {
-        prop: 'scroll',
-        value: 500,
-        duration: 1000        
-    });    
-})
+            for(let condition of x) if(this.option.prop === condition) this.currentValue = (this.currentValue/parentW)*100;
+            for(let condition of y) if(this.option.prop === condition) this.currentValue = (this.currentValue/parentH)*100; 
+            this.option.value = parseFloat(this.option.value);
+        } 
 
-function animate(selector, option){
-    const startTime= performance.now();
-    let currentValue= null;
-    if(option.prop==='scroll'){
-        currentValue = selector.scrollY;
-    }else{
-        currentValue = parseFloat(getComputedStyle(selector)[option.prop]); 
+        if(this.option.value === this.currentValue) return;    
+        requestAnimationFrame(time =>this.run(time));  
     }
-      
 
-    let isString = typeof option.value;
-    if(isString === 'string') {
-        const parentW = parseInt(getComputedStyle(selector.parentElement).width);
-        const parentH = parseInt(getComputedStyle(selector.parentElement).height);
-        const x = ['margin-left','margin-right','left','right','width'];
-        const y = ['margin-top','margin-bottom','top','bottom','height'];
-
-        for(let condition of x) if(option.prop === condition) currentValue = (currentValue/parentW)*100;
-        for(let condition of y) if(option.prop === condition) currentValue = (currentValue/parentH)*100; 
-        option.value = parseFloat(option.value);
-    } 
-
-    if(option.value === currentValue) return;    
-    requestAnimationFrame(run);  
-    
-    function run(time){
-        let timeLast = time - startTime;
-        let progress = timeLast/option.duration;
+    run(time){
+        let timeLast = time - this.startTime;
+        let progress = timeLast/this.option.duration;
     
         if(progress < 0) progress = 0;
         if(progress > 1) progress = 1;
         if(progress < 1){
-            requestAnimationFrame(run); 
+            requestAnimationFrame(time=>this.run(time)); 
         }else{
-            if(option.callback) option.callback();
+            if(this.option.callback) this.option.callback();
         } 
 
-        let result = currentValue + ((option.value-currentValue)*progress); 
+        let result = this.currentValue + ((this.option.value-this.currentValue)*progress); 
         
-        if(isString === 'string'){
-            selector.style[option.prop] = `${result}%`;
+        if(this.isString === 'string'){
+            this.selector.style[this.option.prop] = `${result}%`;
         }
-        else if(option.prop === 'opacity'){
-            selector.style[option.prop] = result;
+        else if(this.option.prop === 'opacity'){
+            this.selector.style[this.option.prop] = result;
         }
-        else if(option.prop === 'scroll'){
+        else if(this.option.prop === 'scroll'){
             window.scroll(0,result);
         }
         else{
-            selector.style[option.prop] = `${result}px`;
+            this.selector.style[this.option.prop] = `${result}px`;
         }
     }
 }
+
+
+    
+    
+    
+
 
